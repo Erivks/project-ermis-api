b2d9662652af542c9d71ea21e6a0b2f0351566d3
Santos <erick_santos00@outlook.com>
2022-05-12 00:16:19 -0300

    feat(logger-middleware): Adding body for log

--- common-api/src/config/middleware/logger.js
+++ common-api/src/config/middleware/logger.js
@@ -4,5 +4,8 @@ import logger from '../../../../../common/functions/logger.js';
 export default function middlewareLogger(req, res, next) {
     var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     logger(LOG_LEVEL.LOG_INFO, `Endpoint called: ${req.path} - IP: ${ip}`);
+    if (req.body) {
+        logger(LOG_LEVEL.LOG_INFO, `Body Request: ${JSON.stringify(req.body)}`);
+    }
     next();
 }
