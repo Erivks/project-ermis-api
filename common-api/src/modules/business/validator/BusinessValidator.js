6ab40e70eddb4ed8b21b03d45f2ec66fc81a5eb9
Santos <erick_santos00@outlook.com>
2022-05-12 00:48:44 -0300

    feat(validator): Add validator for CNPJ param

--- common-api/src/modules/business/validator/BusinessValidator.js
+++ common-api/src/modules/business/validator/BusinessValidator.js
@@ -47,13 +47,27 @@ class BusinessValidator {
         ];
     }
 
-    findByID() {
+    byID() {
+        logger(LOG_LEVEL.LOG_INFO, "Running BusinessValidator::byID");
         return [
             param('id')
                 .notEmpty()
                 .withMessage('param "id" must be present')
                 .isNumeric()
                 .withMessage('param "id" must be numeric')
+        ];
+    }
+
+    byCNPJ() {
+        logger(LOG_LEVEL.LOG_INFO, "Running BusinessValidator::byCNPJ");
+        return [
+            param('cnpj')
+                .notEmpty()
+                .withMessage('param "cnpj" must be present')
+                .isLength({ min: 14 })
+                .withMessage('CNPJ must contains 14 chars')
+                .isNumeric()
+                .withMessage('param "cnpj" must be numeric')
         ];
     }
 }
