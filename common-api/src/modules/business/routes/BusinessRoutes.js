151e5569f83205047a5184ccb3f975c0ea0077f3
Erick O <erick_santos00@outlook.com>
2022-06-07 18:50:11 -0300

    refactor(route): Using a variable for base url

--- common-api/src/modules/business/routes/BusinessRoutes.js
+++ common-api/src/modules/business/routes/BusinessRoutes.js
@@ -3,16 +3,20 @@ import BusinessController from '../controller/BusinessController.js';
 import BusinessValidator from '../validator/BusinessValidator.js';
 
 const router = new Router();
+const baseUrl = "/api/business";
 
 //== CREATE ==
-router.post('/api/business/create', BusinessValidator.create(), BusinessController.create);
+router.post(`${baseUrl}/create`, BusinessValidator.create(), BusinessController.create);
 
 //== READ ==
-router.get('/api/business/findAll', BusinessController.findAll);
-router.get('/api/business/findByID/:id', BusinessValidator.byID(), BusinessController.findByID);
+router.get(`${baseUrl}/findAll`, BusinessController.findAll);
+router.get(`${baseUrl}/findByID/:id`, BusinessValidator.byID(), BusinessController.findByID);
 
 //== UPDATE ==
-router.put('/api/business/updateByID/:id', BusinessValidator.byID(), BusinessController.updateByID);
-router.put('/api/business/updateByCNPJ/:cnpj', BusinessValidator.byCNPJ(), BusinessController.updateByCNPJ);
+// TODO: Validar endpoints
+router.put(`${baseUrl}/updateByID/:id`, BusinessValidator.byID(), BusinessController.updateByID);
+router.put(`${baseUrl}/updateByCNPJ/:cnpj`, BusinessValidator.byCNPJ(), BusinessController.updateByCNPJ);
 
+//== DELETE ==
+// TODO: Desenvolver endpoints de delete
 export default router;
