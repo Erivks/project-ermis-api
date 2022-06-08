5e2602b83ea1ac1443a3bf88e25f878382ba17dc
Erick O <erick_santos00@outlook.com>
2022-06-08 13:34:58 -0300

    feat(update): update validation

--- common-api/src/modules/business/validator/BusinessValidator.js
+++ common-api/src/modules/business/validator/BusinessValidator.js
@@ -70,6 +70,48 @@ class BusinessValidator {
                 .withMessage('param "cnpj" must be numeric')
         ];
     }
+
+    update() {
+        logger(LOG_LEVEL.LOG_INFO, "Running BusinessValidator::update");
+        return [
+            body('cnpj')
+                .isString()
+                .withMessage('CNPJ must be a string')
+                .isLength({ min: 14 })
+                .withMessage('CNPJ must be 14 chars')
+                .optional(),
+            body('name')
+                .isString()
+                .withMessage('name must be a string')
+                .isLength({ min: 1, max: 200 })
+                .withMessage("Length's name must be: [min: 1, max: 200]")
+                .optional(),
+            body('address')
+                .isString()
+                .isLength({ min: 5, max: 200 })
+                .optional(),
+            body('number')
+                .isString()
+                .isLength({ max: 10 })
+                .optional(),
+            body('district')
+                .isString()
+                .isLength({ max: 100 })
+                .optional(),
+            body('city')
+                .isString()
+                .isLength({ max: 100 })
+                .optional(),
+            body('state')
+                .isString()
+                .isLength({ min: 2 })
+                .optional(),
+            body('complement')
+                .isString()
+                .isLength({ max: 150 })
+                .optional()
+        ];
+    }
 }
 
 export default new BusinessValidator();
