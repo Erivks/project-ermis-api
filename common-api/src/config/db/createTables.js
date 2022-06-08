e13840e3b4c9380276c99000f82647310e2d9d97
Erick O <erick_santos00@outlook.com>
2022-06-08 17:50:40 -0300

    feat(tables): Add tbl_responsible to create table

--- common-api/src/config/db/createTables.js
+++ common-api/src/config/db/createTables.js
@@ -1,9 +1,11 @@
 import BusinessModel from "../../modules/business/model/BusinessModel.js";
+import ResponsibleModel from "../../modules/responsible/model/ResponsibleModel.js";
 import { LOG_LEVEL } from '../../../../../common/constants/main.js'
 import logger from '../../../../../common/functions/logger.js';
 
 try {
     await BusinessModel.sync();
+    await ResponsibleModel.sync();
 } catch (error) {
     logger(LOG_LEVEL.LOG_ERR, error.message);
 }
